1d75e6fcee79aeba9d0d0a49fe69bbba40cb153a
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 16:30:36 +0900

    택시 기하학

---
+++ baekjoon/3053.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define Pi 3.14159265358979323846
+using namespace std;
+
+int main()
+{
+	int R;
+	scanf("%d", &R);
+
+	printf("%6f\n", (double)R*R*Pi);
+	printf("%6f\n", (double)2 * R * R);
+}
