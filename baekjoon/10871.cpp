e33e00d545d91e90f54b257b1273bcd03563b0ee
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 17:04:51 +0900

    X보다 작은 수

---
+++ baekjoon/10871.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int arr[10001];
+	int n, x;
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < x)
+			cout << arr[i] << " ";
+	}
+	return 0;
+}
