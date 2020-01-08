bffbe67796fdd059703e4bdc2070cd052b77353f
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 17:34:33 +0900

    N과 M(4)

---
+++ baekjoon/15652.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void solve(int cnt, int n)
+{
+	arr[cnt] = n;
+	if (cnt == M) {
+		for (int i = 1; i <= cnt; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (int i = n; i <= N; i++) {
+		solve(cnt + 1, i);
+	}
+
+}
+
+int main()
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 1; i <= N; i++) {
+		solve(1, i);
+	}
+}
