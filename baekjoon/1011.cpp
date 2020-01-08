d28a435cdad7379c4ff99c59a0506b37c6b13f75
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 14:10:36 +0900

    Create 1011.cpp

---
+++ baekjoon/1011.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+	int x = 0, y = 0;
+
+	scanf("%d", &T);
+
+	for (int t = 1; t <= T; t++) {
+		scanf("%d %d", &x, &y);
+		int dest = y - x;
+		
+		int tmp = 0;
+
+		long long rangeA = 1;
+		long long rangeB = 1;
+		long long i;
+		for (i = 1; i < dest;) {
+			tmp++;
+			i = i + 2 * tmp;
+			rangeA = rangeB;
+			rangeB = i;
+		}
+
+		if (i == dest) {
+			printf("%d\n", 1 + 2 * tmp);
+		}
+
+		else {
+			if (dest - rangeA < rangeB - dest) {
+				printf("%d\n", 2 * tmp - 1);
+			}
+			else {
+				printf("%d\n", 2 * tmp);
+			}
+		}
+	}
+}
