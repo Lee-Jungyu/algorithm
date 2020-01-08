c03d2b76185ad918f9000e7e4a632a0e14e1131b
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 14:09:48 +0900

    Create 1010.cpp

---
+++ baekjoon/1010.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int combination(int n, int m);
+
+int dp[31][31] = { {1,}, };
+int main()
+{
+	int T, n, m;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> n >> m;
+		cout << combination(n, m) << endl;
+	}
+	return 0;
+}
+
+int combination(int n, int m)
+{
+	int cnt;
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			if (i == 1) dp[i][j] = j;
+			else if (i == j) dp[i][j] = 1;
+			else {
+				dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+			}
+		}
+	}
+	return dp[n][m];
+}
