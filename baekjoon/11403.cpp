034a98013750fabb66fc8d1ebe0765298cad8a20
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 17:19:49 +0900

    경로 찾기

---
+++ baekjoon/11403.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int arr[100][100];
+int print[100][100];
+bool visited[100];
+int n;
+
+void dfs();
+int main()
+{
+	cin >> n;
+	int cnt = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		for (int j = 0; j < 100; j++)
+		{
+			arr[i][j] = 0;
+			print[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	dfs();
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << print[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void dfs()
+{
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) visited[i] = false;
+		q.push(i);
+		while (!q.empty())
+		{
+			int w = q.front();
+			for (int j = 0; j < n; j++)
+			{
+				if (arr[w][j] == 1 && !visited[j])
+				{
+					q.push(j);
+					visited[j] = true;
+					print[i][j] = 1;
+				}
+			}
+			q.pop();
+		}
+	}
+}
