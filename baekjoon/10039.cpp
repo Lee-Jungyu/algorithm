3c6728aa8e6feab7e570f4539d592838f30abe6c
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-08 16:55:30 +0900

    평균 점수

---
+++ baekjoon/10039.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0, a;
+	for (int i = 0; i < 5; i++) {
+		cin >> a;
+		if (a < 40) a = 40;
+		sum += a;
+	}
+	cout << sum / 5 << endl;
+}
