52ad1eeac776d8b86450314117555b9d560ca6c7
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-01-11 15:38:50 +0900

    2016년

---
+++ programmers/12901.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string answer[7] = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+    int days[12] = {0,31,29,31,30,31,30,31,31,30,31,30};
+    int sum = 0;
+    for(int i = 0; i < a; i++)
+        sum += days[i];
+    sum+=b;
+    
+    
+    return answer[sum%7];
+}
