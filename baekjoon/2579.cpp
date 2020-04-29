6587287a157ff37b8aae6e3a0e3624c93074394b
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-04-29 14:58:17 +0900

    계단 오르기

--- baekjoon/2579.cpp
+++ baekjoon/2579.cpp
@@ -1,44 +1,46 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int N;
 int stair[301];
 int dp[301][2];
-int result;
 
-int _max(int a, int b)
+int getMax(int a, int b)
 {
-	if (a > b) return a;
-	return b;
+        return a > b ? a : b;
 }
 
 int main()
 {
-	scanf("%d", &N);
+        scanf("%d\n", &N);
+        for(int i = 1; i <= N; i++)
+        {
+                scanf("%d", &stair[i]);
+        }
+        dp[1][0] = stair[1];
+        dp[1][1] = 0;
+        if(N == 1)
+        {
+                printf("%d\n", getMax(dp[1][0], dp[1][1]));
+                return 0;
+        }
+        dp[2][0] = stair[2];
+        dp[2][1] = stair[1]+stair[2];
+        if(N == 2)
+        {
+                printf("%d\n", getMax(dp[2][0], dp[2][1]));
+                return 0;
+        }
 
-	for (int i = 0; i < N; i++)
-		scanf("%d", &stair[i]);
+        else 
+        {
+                for(int i = 3; i <= N; i++)
+                {
+                        dp[i][0] = getMax(dp[i - 2][0], dp[i - 2][1]) + stair[i];
+                        dp[i][1] = dp[i - 1][0] + stair[i];
+                }
 
-	dp[0][0] = stair[0];
-	dp[0][1] = stair[0];
-
-	if (N == 0) {
-		result = dp[0][1];
-		printf("%d\n", result);
-	}
-
-	dp[1][0] = stair[1];
-	dp[1][1] = _max(dp[0][0] + stair[1], dp[1][0]);
-
-	if (N == 1) {
-		result = dp[1][1];
-		printf("%d\n", result);
-	}
-
-	for (int i = 2; i < N; i++) {
-		dp[i][0] = dp[i - 2][1] + stair[i];
-		dp[i][1] = _max(dp[i - 1][0] + stair[i], dp[i][0]);
-	}
-
-	printf("%d\n", dp[N - 1][1]);
+                printf("%d\n", getMax(dp[N][0], dp[N][1]));
+                return 0;
+        }
 }
