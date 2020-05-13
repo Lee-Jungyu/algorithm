8fca5913f56202f41e264809f849279badd09b0c
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-05-13 16:38:57 +0900

    2019 카카오 공채 - 1. 오픈채팅방

--- programmers/kakao/42888.cpp
+++ programmers/kakao/42888.cpp
@@ -1,109 +1,68 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-string id[100000];
-string name[100000];
-int cnt = 0;
-
-string toString(int s) {
-	string result = "";
-	if (s == 0) return "0";
-	while (s != 0) {
-		result = (char)((s % 10) + '0') + result;
-		s /= 10;
-	}
-	return result;
-}
-
 vector<string> solution(vector<string> record) {
-	vector<string> answer;
-	string rid;
-	string nn;
-	for (int i = 0; i < record.size(); i++)
-	{
-		rid = "";
-		nn = "";
-		if (record.at(i)[0] == 'E') { //Enter
-			int j;
-			int idx = cnt;
-			for (j = 6; j < record.at(i).length(); j++) {
-				if (record.at(i)[j] == ' ') { break; }
-				rid += record.at(i)[j];
-			}
-			bool already = false;
-			int k;
-			for (k = 0; k < cnt; k++) {
-				if (id[k] == rid) {
-					already = true;
-					break;
-				}
-			}
-			if (already == false) {
-
-				id[cnt] = rid;
-				int a = j;
-				for (j = a + 1; j < record.at(i).length(); j++) {
-					nn += record.at(i)[j];
-				}
-				name[cnt++] = nn;
-				answer.push_back(toString(idx) + " 님이 들어왔습니다.");
-			}
-			else {
-				for (int l = j + 1; l < record.at(i).length(); l++) {
-					nn += record.at(i)[l];
-				}
-				name[k] = nn;
-				answer.push_back(toString(k) + " 님이 들어왔습니다.");
-			}
-		}
-
-		else if (record.at(i)[0] == 'L') { //Leave
-			for (int j = 6; j < record.at(i).length(); j++) {
-				if (record.at(i)[j] == ' ') { break; }
-				rid += record.at(i)[j];
-			}
-			int idx = 0;
-			for (int j = 0; j < cnt; j++) {
-				if (id[j] == rid) {
-					idx = j;
-				}
-			}
-			answer.push_back(toString(idx) + " 님이 나갔습니다.");
-		}
-
-		else { //Change
-			int j;
-			for (j = 7; j < record.at(i).length(); j++) {
-				if (record.at(i)[j] == ' ') { break; }
-				rid += record.at(i)[j];
-			}
-			for (int k = 0; k < cnt; k++) {
-				if (id[k] == rid) {
-					for (int l = j + 1; l < record.at(i).length(); l++) {
-						nn += record.at(i)[l];
-					}
-					name[k] = nn;
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < answer.size(); i++) {
-		string str = answer.at(i);
-		int j;
-		for (j = 0; j < str.length(); j++) {
-			if (str[j] == ' ') {
-				break;
-			}
-		}
-		string str1 = str.substr(0, j + 0);
-		string str2 = str.substr(j + 1);
-
-		str = (name[stoi(str1)] + str2);
-		answer.at(i) = str;
-	}
-
-	return answer;
+    vector<string> answer;
+    vector<pair<string, string>> temp; 
+    map<string, string> recordmap;
+    
+    for(int i = 0; i < record.size(); i++) {
+        string id = "";
+        bool emptyFlag = false;
+        int id_start_idx = 0;
+        int id_last_idx = 0;
+        for(int j = 0; j < record[i].length(); j++) {
+            if(!emptyFlag &&record[i][j] == ' ') {
+                id_start_idx = j + 1;
+                emptyFlag = true;
+            }
+            else if(emptyFlag && record[i][j] == ' ') {
+                id_last_idx = j - 1;
+                break;
+            }
+        }
+        
+        id = record[i].substr(id_start_idx, id_last_idx - id_start_idx + 1);
+        
+        if(record[i][0] == 'E') {
+            //Enter
+            string nn = "";
+            for(int j = record[i].length() - 1; j >= 0; j--) {
+                if(record[i][j] == ' ') break;
+                nn = record[i][j] + nn;
+            }
+            
+            temp.push_back(make_pair("Enter", id));
+            recordmap[id] = nn;
+        }
+        else if (record[i][0] == 'L') {
+            //Leave
+            temp.push_back(make_pair("Leave", id));
+        }
+        else {
+            //Change
+            string nn = "";
+            for(int j = record[i].length() - 1; j >= 0; j--) {
+                if(record[i][j] == ' ') break;
+                nn = record[i][j] + nn;
+            }
+            
+            recordmap[id] = nn;
+        }
+    }
+    
+    for(int i = 0; i <temp.size(); i++) {
+        string str = "";
+        if(temp[i].first == "Enter") {
+            str = recordmap[temp[i].second] + "님이 들어왔습니다.";
+        }
+        else {
+            str = recordmap[temp[i].second] + "님이 나갔습니다.";
+        }
+        answer.push_back(str);
+    }
+    return answer;
 }
