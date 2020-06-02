554f69ee4e274cf30dccd1c5cf508d302abd6ad5
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-06-02 14:48:12 +0900

    level1 - 가운데 글자 가져오기

---
+++ programmers/12903.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    if(s.size() % 2) answer += s[s.size()/2];
+    else answer = answer + s[s.size()/2 - 1] + s[s.size()/2];
+    return answer;
+}
