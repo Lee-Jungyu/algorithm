cd6e54e378f3f4f5e83f2e63a3a786a3274d2228
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-06-18 11:34:21 +0900

    level3 - 입국심사

---
+++ programmers/43238.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    
+    sort(times.begin(), times.end());
+    
+    long long _min = times[0];
+    long long _max = times[times.size() - 1] * n;
+    long long _tmp = 0;
+    while(true) {
+        long long _middle = (_min + _max) / 2;
+        long long sum = 0;
+        
+        for(int i = 0; i < times.size(); i++) sum += _middle / times[i];
+        
+        if(_min > _max) return _min;
+        
+        if(n > sum) _min = _middle + 1;
+        else _max = _middle - 1;
+        
+        _tmp = _middle;
+    }
+    
+    return answer;
+}
+//이분탐색
