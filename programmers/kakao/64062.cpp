5629ef4d2964ba4c2be5792873ea471747b45edd
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2020-09-03 16:53:53 +0900

    2019 카카오 개발자 겨울 인턴십 - 5. 징겁다리 건너기

---
+++ programmers/kakao/64062.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> stones, int k) {
+    int min = 1;
+    int max = 200000000;
+    int tmp = 0;
+    
+    while(true) {
+        int mid = (min + max) / 2;
+        
+        if(mid == tmp) break;
+        tmp = mid;
+        
+        int cnt = 0;
+        bool check = false;
+        
+        for(int i = 0; i < stones.size(); i++) {
+            if(stones[i] < mid) {
+                cnt++;
+            }
+            else {
+                cnt = 0;
+            }
+            
+            if(cnt == k) {
+                check = true;
+                break;
+            }
+        }
+        
+        if(check) {
+            max = mid - 1;
+        }
+        else {
+            min = mid + 1;
+        }
+    }
+    
+    return tmp;
+}
