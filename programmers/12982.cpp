bf9e24c6d6545af382df43bd5b4d414ab25766a5
Lee-Jungyu <38247004+Lee-Jungyu@users.noreply.github.com>
2021-05-09 23:20:39 +0900

    level1 - 예산
    정렬

---
+++ programmers/12982.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(), d.end());
+    
+    for(int i = 0; i < d.size(); i++) 
+    {
+        if(budget - d[i] < 0) break;
+        budget -= d[i];
+        answer++;
+    }
+    
+    return answer;
+}
